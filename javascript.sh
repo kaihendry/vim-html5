cff5b1cdedb7e05d4ebd3d8c01d5043924722f58
Kai Hendry <hendry@iki.fi>
2011-11-28 17:10:00 +0800

    jshint

--- javascript.sh
+++ javascript.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 test -f "$1" || exit
-curl -s -F js=@$1 http://jslint.dabase.com/ |
-sed 's,^,'"$1:"','
+curl -s -F js=@$1 http://jslint.dabase.com/jshint.php | sed 's,^/tmp/tmp.*:,'"$1:"',' | sed '/^$/d' | sed '/.*error/d'
